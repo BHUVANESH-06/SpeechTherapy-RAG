5d4e56969dadb96927a839c6b27039b59682196d
BHUVANESH-06 <bhuvanesh.g666@gmail.com>
2025-07-28 19:05:49 +0530

    fix: changed creating embeddings from creating every time to once and stored in pkl

--- backend/ingestion/ingest.py
+++ backend/ingestion/ingest.py
@@ -1,20 +1,7 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from ingestion.loader import load_pdfs
-from langchain import hub
-from langchain_community.vectorstores import InMemoryVectorStore
-from langchain_community.embeddings import HuggingFaceEmbeddings
+import os
+import pickle
 
+pkl_path = os.path.join(os.path.dirname(__file__), "vector_store.pkl")
 
-from dotenv import load_dotenv
-load_dotenv()
-
-docs = load_pdfs()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-all_splits = text_splitter.split_documents(docs)
-
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-vector_store = InMemoryVectorStore(embeddings)
-
-_ = vector_store.add_documents(documents=all_splits)
-
+with open(pkl_path, "rb") as f:
+    vector_store = pickle.load(f)
--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from graph.build_graph import graph
 from context_manager.memory import memory
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 app.add_middleware(
@@ -12,16 +14,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class Message(BaseModel):
+    type: str
+    text: str
+
+class ChatRequest(BaseModel):
+    query: str
+    chat_history: List[Message]
 
 print("TherapyBot is ready! Ask your therapy questions:\n")
 
-@app.get("/chat", response_model=str)
-def chat(query: str):
+@app.post("/chat", response_model=str)
+def chat(data: ChatRequest):
+    query = data.query.strip()
+    history = [
+        {"type": msg.type, "content": msg.text} for msg in data.chat_history if msg.text.strip()
+    ]
     print(f"[USER INPUT] {query}")
     try:
         if query.lower() in ["exit", "quit"]:
             return "Goodbye!"
-        memory_vars = memory.load_memory_variables({})
+        memory_vars = {
+            "chat_history": history
+        }
         result = graph.invoke({
             "input": query,
             "memory": memory_vars,
