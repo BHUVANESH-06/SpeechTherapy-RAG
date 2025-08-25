9c6215bdf20b831f458337a60ea5377738253d61
BHUVANESH-06 <bhuvanesh.g666@gmail.com>
2025-08-25 19:38:18 +0530

    feat: huggingFace

--- backend/ingestion/ingest.py
+++ backend/ingestion/ingest.py
@@ -1,7 +1,26 @@
 import os
 import pickle
+import requests
+from dotenv import load_dotenv
 
-pkl_path = os.path.join(os.path.dirname(__file__), "vector_store.pkl")
+load_dotenv()
 
-with open(pkl_path, "rb") as f:
-    vector_store = pickle.load(f)
+
+def load_vector_store():
+    url = os.getenv("VECTOR_STORE_URL")
+
+    pkl_path = "/tmp/vector_store.pkl"
+
+    if not os.path.exists(pkl_path):
+        print("Downloading vector_store.pkl from Hugging Face...")
+        resp = requests.get(url)
+        resp.raise_for_status()
+        with open(pkl_path, "wb") as f:
+            f.write(resp.content)
+
+    with open(pkl_path, "rb") as f:
+        vector_store = pickle.load(f)
+
+    return vector_store
+
+vector_store = load_vector_store()
